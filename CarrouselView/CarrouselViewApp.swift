b287c6450e9784998594826f07ee0ce10f96fd81
Christian Manzaraz <magnusmanz@gmail.com>
2024-02-09 19:07:06 -0300

    Initial Commit

---
+++ CarrouselView/CarrouselViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarrouselViewApp.swift
+//  CarrouselView
+//
+//  Created by Christian Manzaraz on 09/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct CarrouselViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
